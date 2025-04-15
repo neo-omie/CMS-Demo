1f90df6eef6ee18240034cb33cb25113be3e5529
Sarthak <sarthak.lembhe@neosoftmail.com>
2025-04-15 16:42:20 +0530

    commit

--- API/CMS.API/Controllers/AuthController.cs
+++ API/CMS.API/Controllers/AuthController.cs
@@ -10,7 +10,7 @@ namespace CMS.API.Controllers
         [HttpGet]
         public IActionResult test()
         {
-            return Ok("Test");
+            return Ok("Mukesh");
         }
     }
 }
