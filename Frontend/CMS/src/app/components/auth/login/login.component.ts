5d32855429627b078f87c6c7d9d516ee9564000b
Om <autiom3123@gmail.com>
2025-04-17 10:26:13 +0530

    commit

--- Frontend/CMS/src/app/components/auth/login/login.component.ts
+++ Frontend/CMS/src/app/components/auth/login/login.component.ts
@@ -3,6 +3,8 @@ import { AuthResponse, Login } from '../../../models/auth/login';
 import { UserService } from '../../../services/auth/user.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { TYPE } from './values.constants';
 
 @Component({
   selector: 'app-login',
@@ -26,22 +28,46 @@ export class LoginComponent {
         localStorage.setItem('token', response.token);
         localStorage.setItem('email', response.email);
         localStorage.setItem('name', response.name);
-        alert('You are successfully logged in!');
+        // alert('You are successfully logged in!');
+        this.toast(TYPE.SUCCESS, true, 'Signed in successfully');
         
       }, error:(error) => {
         console.error('Login Failed :(', error);
+<<<<<<< Updated upstream
         if(error.error !== undefined){
           this.errorMsg = JSON.stringify(error.error.message);
           
         }
         else{
           this.errorMsg = JSON.stringify(error.message);
+=======
+        if(error.message !== undefined){
+          this.errorMsg = JSON.stringify(error.error.message);
+          this.toast(TYPE.ERROR, true, error.error.message);
         }
-        alert(this.errorMsg);
+        else{
+          this.errorMsg = JSON.stringify(error.message);
+          this.toast(TYPE.ERROR, true, error.message);
+>>>>>>> Stashed changes
+        }
+        // alert(this.errorMsg);
       }
     });
   }
+<<<<<<< Updated upstream
   loginPasswordEyeToggle(){
     this.loginPasswordEyeOpen = !this.loginPasswordEyeOpen;
+=======
+  toast(typeIcon = TYPE.SUCCESS, timerProgressBar: boolean = false, op:string = "") {
+    Swal.fire({
+      toast: true,
+      position: 'top',
+      showConfirmButton: false,
+      icon: typeIcon,
+      timerProgressBar,
+      timer: 5000,
+      title: op
+    })
+>>>>>>> Stashed changes
   }
 }
