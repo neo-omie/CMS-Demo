4b09ebf4d046ecd66176560033bf931ef705b33e
Sakthish-Jayakumar-Nadar <sakthishjayakumarnadar007@gmail.com>
2025-04-24 18:19:00 +0530

    paginator global done

--- Frontend/CMS/src/app/services/approver-matrix-contract.service.ts
+++ Frontend/CMS/src/app/services/approver-matrix-contract.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ApprovalMatrixContract } from '../models/approval-matrix-contract';
+import { ApprovalMatrixContract, EditApprovalMatrixContractDto } from '../models/approval-matrix-contract';
 import { Observable } from 'rxjs';
 import { MasterEmployee } from '../models/master-employee';
 
@@ -18,5 +18,8 @@ export class ApproverMatrixContractService {
     }
     GetApproversForInputText(departmentId: number, inputText:string):Observable<MasterEmployee[]>{
       return this.http.get<MasterEmployee[]>(`https://localhost:7041/api/Employee/search/${departmentId}/${inputText}`)
+    }
+    EditApproverMatrixContract(id:number,editApprovalMatrixContractDto:EditApprovalMatrixContractDto):Observable<boolean>{
+      return this.http.post<boolean>(`${this.apiUrl}/${id}`,editApprovalMatrixContractDto);
     }
 }
