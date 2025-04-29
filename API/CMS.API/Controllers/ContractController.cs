4bd110fca841bb84b0f7538bd680d19c138519a8
Om <autiom3123@gmail.com>
2025-04-29 13:00:02 +0530

    Added logging in Contract Controller

--- API/CMS.API/Controllers/ContractController.cs
+++ API/CMS.API/Controllers/ContractController.cs
@@ -25,34 +25,44 @@ namespace CMS.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllContracts(int pageNumber, int pageSize)
         {
+            _logger.LogInformation("GetAllContracts method initiated");
             var allContracts = await _mediator.Send(new GetAllContractsQuery(pageNumber, pageSize));
+            _logger.LogInformation("GetAllContracts method performed");
             return Ok(allContracts);
         }
         [Route("{id}")]
         [HttpGet]
         public async Task<IActionResult> GetContractById([FromRoute]int id)
         {
+            _logger.LogInformation("GetContractById method initiated");
             var foundContract = await _mediator.Send(new GetContractByIdQuery(id));
+            _logger.LogInformation("GetContractById method performed");
             return Ok(foundContract);
         }
         [HttpPost]
         public async Task<IActionResult> AddContract(ContractDTO cont)
         {
+            _logger.LogInformation("AddContract method initiated");
             var addedContract = await _mediator.Send(new CreateNewContractCommand(cont));
+            _logger.LogInformation("AddContract method performed");
             return Ok(addedContract);
         }
         [Route("{id}")]
         [HttpPut]
         public async Task<IActionResult> UpdateContract([FromRoute]int id, [FromBody]ContractDTO cont)
         {
+            _logger.LogInformation("UpdateContract method initiated");
             var editedContract = await _mediator.Send(new EditContractCommand(id, cont));
+            _logger.LogInformation("UpdateContract method performed");
             return Ok(editedContract); // bool
         }
         [Route("{id}")]
         [HttpDelete]
         public async Task<IActionResult> DeleteContract([FromRoute] int id)
         {
+            _logger.LogInformation("DeleteContract method initiated");
             var deletedContract = await _mediator.Send(new DeleteContractCommand(id));
+            _logger.LogInformation("DeleteContract method performed");
             return Ok(deletedContract); // bool
         }
     }
