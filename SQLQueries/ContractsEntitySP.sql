40fd8841a8c47de1ff3935b46cb1d1c055e1a199
Om <autiom3123@gmail.com>
2025-04-30 19:11:02 +0530

    contract entity sp made adding in repo remaining

---
+++ SQLQueries/ContractsEntitySP.sql
@@ -0,0 +1,58 @@
+CREATE OR ALTER PROCEDURE SP_GetAllContracts @PageNumber int, @PageSize int
+AS
+DECLARE @TotalRecords int
+BEGIN
+	SELECT @TotalRecords = COUNT(ContractId) FROM ContractsEntity
+
+	SELECT c.ContractId as ContractID, c.ContractName as ContractName,
+	cc.ContractTypeName as ContractType, dd.DepartmentName as DepartmentName,
+	c.ValidFrom as EffectiveDate, c.ValidTill as ExpiryDate,
+	c.RenewalFrom as ToBeRenewedOn, c.RenewalTill as AddendumDate,
+	c.Approver3Status as Status, me.EmployeeName as ApprovalPendingFrom,
+	me.EmployeeName as RenewalContractPerson, CAST(CAST((CAST(c.RenewalTill as datetime) - GETDATE()) as int) as nvarchar(50)) as RenewalDueIn,
+	c.Location as Location, @TotalRecords as TotalRecords
+	FROM ContractsEntity c
+	LEFT JOIN contracts cc ON cc.ValueId = c.ContractTypeId
+	LEFT JOIN MasterApprovalMatrixContracts d ON d.DepartmentId = c.DepartmentId
+	LEFT JOIN Departments dd ON dd.DepartmentId = c.DepartmentId
+	LEFT JOIN MasterEmployees me ON me.EmployeeCode = d.ApproverId3
+	WHERE c.IsDeleted = 0
+	ORDER BY c.ContractId
+	OFFSET(@PageNumber-1)*@PageSize ROWS
+	FETCH NEXT @PageSize ROWS ONLY
+END
+EXEC SP_GetAllContracts @PageNumber = 1, @PageSize = 10;
+
+SELECT CAST(CAST((CAST(c.RenewalTill as datetime) - GETDATE()) as int) as nvarchar(50)) as RenewalDueIn FROM ContractsEntity c;
+SELECT CAST(RenewalTill as datetime) FROM ContractsEntity;
+
+CREATE OR ALTER PROCEDURE SP_GetContractByID @ID int
+AS
+BEGIN
+	SELECT c.ContractId as ContractID, c.ContractName as ContractName,
+	c.DepartmentId as DepartmentId, dd.DepartmentName as DepartmentName,
+	c.ContractWithCompanyId as ContractWithCompanyId, mc.CompanyName as ContractWithCompanyName,
+	c.ContractTypeId as ContractTypeId, cc.ContractTypeName as ContractType,
+	c.ApostilleTypeId as ApostilleTypeId, ma.ApostilleName as ApostilleTypeName,
+	c.ActualDocRefNo as ActualDocRefNo, c.RetainerContract as RetainerContract,
+	c.TermsAndConditions as TermsAndConditions,
+	c.ValidFrom as ValidFrom, c.ValidTill as ValidTill,
+	c.RenewalFrom as RenewalFrom, c.RenewalTill as RenewalTill,
+	c.AddendumDate as AddendumDate,
+	c.EmpCustodianId as EmpCustodianId, me.EmployeeName as EmpCustodianName,
+	c.Location as Location, c.Approver1Status as Approver1Status,
+	c.Approver2Status as Approver2Status, c.Approver3Status as Approver3Status,
+	c.IsDeleted as IsDeleted
+	FROM ContractsEntity c
+	LEFT JOIN contracts cc ON cc.ValueId = c.ContractTypeId
+	LEFT JOIN MasterApprovalMatrixContracts d ON d.DepartmentId = c.DepartmentId
+	LEFT JOIN Departments dd ON dd.DepartmentId = c.DepartmentId
+	LEFT JOIN MasterEmployees me ON me.EmployeeCode = d.ApproverId3
+	LEFT JOIN MasterCompanies mc ON mc.ValueId = c.ContractWithCompanyId
+	LEFT JOIN MasterApostilles ma ON ma.ValueId = c.ApostilleTypeId
+	WHERE c.ContractId = @ID;
+END
+EXEC SP_GetContractByID @ID = 2;
+
+SELECT CAST(CAST((CAST(c.RenewalTill as datetime) - GETDATE()) as int) as nvarchar(50)) as RenewalDueIn FROM ContractsEntity c;
+SELECT CAST(RenewalTill as datetime) FROM ContractsEntity;
