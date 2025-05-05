378c8f55c9bcd74cf9d21005322236bb97a9b42e
Sarthak <sarthak.lembhe@neosoftmail.com>
2025-05-05 16:19:50 +0530

    in progress company update

--- Frontend/CMS/src/app/app.routes.ts
+++ Frontend/CMS/src/app/app.routes.ts
@@ -23,6 +23,8 @@ import { AddContractComponent } from './components/contracts/add-contract/add-co
 import { ClassifiedContractsScreenComponent } from './components/classifiedContracts/classified-contracts-screen/classified-contracts-screen.component';
 import { AllClassifiedContractComponent } from './components/classifiedContracts/all-classified-contract/all-classified-contract.component';
 import { CreateClassifiedContractComponent } from './components/classifiedContracts/create-classified-contract/create-classified-contract.component';
+import { MasterApostilleComponent } from './components/master-apostille/master-apostille.component';
+import { MasterCompanyUpdateFormComponent } from './components/master-company-update-form/master-company-update-form.component';
 
 export const routes: Routes = [
     {path: '', component: LoginScreenComponent}, 
@@ -41,6 +43,7 @@ export const routes: Routes = [
     {path: 'masters/escalationContracts', component: EscalationMatrixContractComponent, canActivate:[authGuard]},
     {path:'masters/escalationMOUs', component: EscalationMatrixMouComponent, canActivate:[authGuard]},
     {path: 'masters/companyMasters/addCompany', component: MasterCompanyAddFormComponent, canActivate:[authGuard]},
+    {path: 'masters/companyMasters/UpdateCompany', component: MasterCompanyUpdateFormComponent, canActivate:[authGuard]},
     {path: 'masters/companyMasters', component: MasterCompanyComponent, canActivate:[authGuard]},
     {path: 'masters/contractTypeMasters', component: ContractTypeMasterComponent, canActivate:[authGuard]},
 
