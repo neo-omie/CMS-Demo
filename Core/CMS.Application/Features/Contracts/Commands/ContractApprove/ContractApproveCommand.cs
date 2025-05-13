044fa0be8a007ff9eb1a4f7f7a0835417d4df69e
Sakthish-Jayakumar-Nadar <sakthishjayakumarnadar007@gmail.com>
2025-05-13 11:23:01 +0530

    Approver Mail functionality

---
+++ Core/CMS.Application/Features/Contracts/Commands/ContractApprove/ContractApproveCommand.cs
@@ -0,0 +1,7 @@
+﻿using CMS.Domain.Entities;
+using MediatR;
+
+namespace CMS.Application.Features.Contracts.Commands.ContractApproveApprover
+{
+    public record ContractApproveCommand(int id, string empCode) : IRequest<Contract>;
+}
---
+++ Core/CMS.Application/Features/Contracts/Commands/ContractApprove/ContractApproveCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CMS.Application.Contracts.Persistence;
+using CMS.Application.Features.Contracts.Commands.ContractApproveApprover;
+using CMS.Application.Features.Contracts.Commands.CreateNewContract;
+using CMS.Domain.Entities;
+using MediatR;
+
+namespace CMS.Application.Features.Contracts.Commands.ContractApprove
+{
+    public class ContractApproveCommandHandler : IRequestHandler<ContractApproveCommand, Contract>
+    {
+            readonly IContractRepository _contractRepository;
+            public ContractApproveCommandHandler(IContractRepository contractRepository)
+            {
+                _contractRepository = contractRepository;
+            }
+            public async Task<Contract> Handle(ContractApproveCommand request, CancellationToken cancellationToken)
+            {
+                return await _contractRepository.ApproveContract(request.id, request.empCode);
+            }
+        
+    }
+}
