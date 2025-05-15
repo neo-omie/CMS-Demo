e0a335ff4d151a7e015a3c1aaf8174434369e9cb
Sarthak <sarthak.lembhe@neosoftmail.com>
2025-05-15 15:10:44 +0530

    commiting

--- Infrastructure/CMS.Persistence/Repositories/PostTerminationRepository.cs
+++ Infrastructure/CMS.Persistence/Repositories/PostTerminationRepository.cs
@@ -37,7 +37,7 @@ namespace CMS.Persistence.Repositories
             var checkContract = await _context.ContractsEntity.FirstOrDefaultAsync(c => c.ContractId == contractId);
             if (checkContract == null)
                 throw new NotFoundException($"Contract with id {contractId} not found");
-            if(checkContract.Approver3Status != ContractStatus.Active)
+            if (checkContract.Approver3Status != ContractStatus.Active)
             {
                 throw new Exception("Cannot terminate the contract as the contract has not been active.");
             }
@@ -84,7 +84,7 @@ namespace CMS.Persistence.Repositories
                 DisplayDocumentName = originalFileName,
                 Notice_Duration = _terminationDocumentUploadDto.Notice_Duration,
                 Remark = _terminationDocumentUploadDto.Remark,
-                End_Date = _terminationDocumentUploadDto.End_Date 
+                End_Date = _terminationDocumentUploadDto.End_Date
 
             };
 
@@ -97,19 +97,19 @@ namespace CMS.Persistence.Repositories
             string sql = "EXEC SP_GetContractEntityByID @ID = {0}";
             var findingContract = await _context.GetContractByIdDtos.FromSqlRaw(sql, document.ContractId).AsNoTracking().ToListAsync();
             var forNotif = findingContract.FirstOrDefault();
-            if ( _context.SaveChanges()>0)
+            if (_context.SaveChanges() > 0)
             {
                 await AddNewNotifications(forNotif.EmpCustodianCode,
                                           $"Notice for Termination of contract '{forNotif.ContractName}'!",
                                           $"NOTICE: Termination for the contract ID {forNotif.ContractId} is initialized. Please check the portal.");
                 await SendMail(
-                    forNotif.EmpCustodianEmail, forNotif.EmpCustodianCode, forNotif.ContractId, forNotif.ContractName,document.DocumentPath
+                    forNotif.EmpCustodianEmail, "Termination Notice Post Contract! 🚨", GenerateEmailBody(forNotif.EmpCustodianCode, forNotif.ContractId, forNotif.ContractName), forNotif.EmpCustodianCode, forNotif.ContractId, forNotif.ContractName, document.DocumentPath
                 );
                 await AddNewNotifications(forNotif.Approver1EmployeeCode,
                                           $"Notice for Termination of contract '{forNotif.ContractName}'!",
                                           $"NOTICE: Termination for the contract ID {forNotif.ContractId} is initialized. Please check the portal.");
                 await SendMail(
-                    forNotif.Approver1Email, forNotif.Approver1EmployeeCode, forNotif.ContractId, forNotif.ContractName, document.DocumentPath
+                    forNotif.Approver1Email, "Termination Notice Post Contract! 🚨", GenerateEmailBody(forNotif.Approver1EmployeeCode, forNotif.ContractId, forNotif.ContractName), forNotif.Approver1EmployeeCode, forNotif.ContractId, forNotif.ContractName, document.DocumentPath
                 );
 
                 return true;
@@ -119,7 +119,7 @@ namespace CMS.Persistence.Repositories
                 throw new Exception("For Some reasons, Document not uploaded");
             }
 
-                
+
         }
         private async Task AddNewNotifications(string name, string subject, string message)
         {
@@ -148,16 +148,137 @@ namespace CMS.Persistence.Repositories
             emailBody += "</div>";
             return emailBody;
         }
-        public async Task SendMail(string email, string name, int contractID, string contractName,string attachmentpath)
+        public async Task SendMail(string email, string subject, string emailBody, string name, int contractID, string contractName, string attachmentpath)
         {
             var mailRequest = new MailRequestWithAttachment
             {
                 Email = email,
-                Subject = "Termination Notice Post Contract! 🚨",
-                EmailBody = GenerateEmailBody(name, contractID, contractName),
-                Attachments=attachmentpath
+                Subject = subject,
+                EmailBody = emailBody,
+                Attachments = attachmentpath
             };
             await _emailService.SendEmailWithAttachment(mailRequest);
+        }
+
+        public async Task<Contract> ApproveTerminateContract(int id, string empCode, ContractStatus status, string subject, string emailBody)
+        {
+            var contract = await _context.ContractsEntity.Where(c => c.ContractId == id).FirstOrDefaultAsync();
+            var forDoc = await _context.PostTerminationNotices.FirstOrDefaultAsync(c => c.ContractId == id);
+            
+            if (contract ==null)
+            {
+                throw new NotFoundException("Contract not found");
+            }
+            string sql = "EXEC SP_GetContractEntityByID @ID = {0}";
+            var findingContract = await _context.GetContractByIdDtos.FromSqlRaw(sql, id).AsNoTracking().ToListAsync();
+            var foundContract = findingContract.FirstOrDefault();
+
+            if (foundContract==null)
+            {
+                throw new NotFoundException("Cotract Not Found");
+
+            }
+
+            string approveOrTerminate = (status == ContractStatus.Terminated) ? "Approved" : "Terminated";
+            string notificationSubject=(status==ContractStatus.Terminated)? "Contract has been approved under your department. You can access and change the approvals for this contract." : "Contract has been Terminated under your department.";
+
+            if (foundContract.Approver1Email==empCode)
+            {
+                if (contract.Approver1Status!=ContractStatus.PendingTermination)
+                {
+                    throw new Exception("Invalid approval action");
+                }
+
+                contract.Approver1Status = status;
+
+                if (await _context.SaveChangesAsync()<=0)
+                {
+                    throw new Exception ("For some reasons contract is not updated");
+                }
+
+                await AddNewNotifications(foundContract.Approver2EmployeeCode, notificationSubject, $"contract called'{foundContract.ContractName} {approveOrTerminate} by '{foundContract.Approver1EmployeeCode}'(Approver 1)!");
+
+                await SendMail(foundContract.Approver2Email, subject, emailBody, foundContract.Approver2EmployeeCode, id, foundContract.ContractName, forDoc.DocumentPath
+                    );
+
+                await AddNewNotifications(foundContract.EmpCustodianCode, notificationSubject.Split('.')[0] + "'",
+                                          $"Contract called '{foundContract.ContractName}'{approveOrTerminate} by '{foundContract.Approver1EmployeeCode}'(Approver 1)!");                                                
+                
+                await SendMail(foundContract.EmpCustodianEmail, subject, emailBody, foundContract.EmpCustodianCode, id, foundContract.ContractName, forDoc.DocumentPath
+                    );
+
+            }
+            else if (foundContract.Approver2Email == empCode)
+            {
+                if(contract.Approver1Status!=ContractStatus.Terminated || contract.Approver2Status != ContractStatus.PendingTermination)
+                {
+                    throw new Exception("Invalid approval action");
+                }
+
+                contract.Approver2Status = status;
+
+                if (await _context.SaveChangesAsync()<=0)
+                {
+                    throw new Exception("For some reasons, Contract is not approved");
+                }
+
+                await AddNewNotifications(foundContract.Approver3EmployeeCode, notificationSubject, $"Contract called '{foundContract.ContractName}' " +
+                    $"{approveOrTerminate} by {foundContract.Approver2EmployeeCode}'" +
+                    $"(Approver 2)!");
+
+                await SendMail(foundContract.Approver3Email, subject, emailBody, foundContract.Approver3EmployeeCode, id, foundContract.ContractName, forDoc.DocumentPath
+                   );
+
+                await AddNewNotifications(foundContract.EmpCustodianCode, notificationSubject.Split('.')[0] + "'",
+                    $"Contract called '{foundContract.ContractName}' {approveOrTerminate} by '{foundContract.Approver2EmployeeCode}' (Approver 2)!");
+
+                
+                await SendMail(foundContract.EmpCustodianEmail, subject, emailBody, foundContract.EmpCustodianCode, id, foundContract.ContractName, forDoc.DocumentPath
+                    );
+            }
+            else if(foundContract.Approver3Email==empCode)
+            {
+                if (contract.Approver2Status != ContractStatus.Terminated || contract.Approver1Status!=ContractStatus.Terminated || contract.Approver3Status != ContractStatus.PendingTermination)
+                {
+                    throw new Exception("Invalid approval action");
+                }
+
+                contract.Approver2Status = status;
+
+                if (await _context.SaveChangesAsync()<=0)
+                {
+                    throw new Exception("For some reaons , contract is not approved");
+                }
+
+                //to approver 1
+                await AddNewNotifications(foundContract.Approver1EmployeeCode, notificationSubject, $"Contract Called '{foundContract.ContractName}' " +
+                    $"{approveOrTerminate} by {foundContract.Approver3EmployeeCode}'" +
+                    $"(Approver 3)!");
+
+                await SendMail(foundContract.Approver1Email, subject, emailBody, foundContract.Approver1EmployeeCode, id, foundContract.ContractName, forDoc.DocumentPath);
+
+                //to approver 2
+                await AddNewNotifications(foundContract.Approver2EmployeeCode, notificationSubject, $"Contract Called '{foundContract.ContractName}' " +
+                   $"{approveOrTerminate} by {foundContract.Approver3EmployeeCode}'" +
+                   $"(Approver 3)!");
+
+                await SendMail(foundContract.Approver2Email, subject, emailBody, foundContract.Approver2EmployeeCode, id, foundContract.ContractName, forDoc.DocumentPath);
+
+                await AddNewNotifications(foundContract.EmpCustodianCode, notificationSubject.Split('.')[0] + "'",
+                                         $"Contract called '{foundContract.ContractName}'{approveOrTerminate} by '{foundContract.Approver2EmployeeCode}'(Approver 3)!");                                              
+
+                
+                await SendMail(foundContract.EmpCustodianEmail, subject, emailBody, foundContract.EmpCustodianCode, id, foundContract.ContractName, forDoc.DocumentPath
+                    );
+
+            }
+            else
+            {
+                throw new Exception("Unauthorized Action");
+            }
+
+            return contract;
+
         }
 
 
