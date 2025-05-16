6f6fe8ab40cfeb049f018f1bb212024b0b239dd0
Om <autiom3123@gmail.com>
2025-05-16 17:01:52 +0530

    notice withdrawal fe/be done

---
+++ Frontend/CMS/src/app/models/notice-withdrawal.spec.ts
@@ -0,0 +1,7 @@
+import { NoticeWithdrawal } from './notice-withdrawal';
+
+describe('NoticeWithdrawal', () => {
+  it('should create an instance', () => {
+    expect(new NoticeWithdrawal()).toBeTruthy();
+  });
+});
