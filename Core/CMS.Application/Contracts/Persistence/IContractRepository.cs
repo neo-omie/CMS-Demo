8ae9a6f4753dcdd94e65d196d95d819795f28f02
Om <autiom3123@gmail.com>
2025-05-21 18:32:06 +0530

    Added contractscount for dashboard; cqrs remaining

--- Core/CMS.Application/Contracts/Persistence/IContractRepository.cs
+++ Core/CMS.Application/Contracts/Persistence/IContractRepository.cs
@@ -12,6 +12,7 @@ namespace CMS.Application.Contracts.Persistence
 {
     public interface IContractRepository
     {
+        //Task<ContractsCount> GetContractsCountAsync();
         Task<IEnumerable<GetAllContractsDto>> GetAllContractsAsync(int pageNumber, int pageSize);
         Task<IEnumerable<GetAllContractsDto>> GetActiveContractsAsync(int pageNumber, int pageSize);
         Task<IEnumerable<GetAllContractsDto>> GetTerminatedContractsAsync(int pageNumber, int pageSize);
