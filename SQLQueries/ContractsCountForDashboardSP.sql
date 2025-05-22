f2d9945509f080c41de5c376aad764862ccd988a
Om <autiom3123@gmail.com>
2025-05-22 10:46:09 +0530

    contracts count added be

--- SQLQueries/ContractsCountForDashboardSP.sql
+++ SQLQueries/ContractsCountForDashboardSP.sql
@@ -1,12 +1,29 @@
 SELECT * FROM ContractsEntity
-CREATE PROCEDURE SP_ContractsCounts
+CREATE OR ALTER PROCEDURE SP_ContractsCounts
 AS
 BEGIN
-	SELECT COUNT(*) AS AllContractsCount FROM ContractsEntity;
-	SELECT COUNT(*) AS PendingApprovalContractsCount FROM ContractsEntity WHERE Approver3Status = 1;
-	SELECT COUNT(*) AS PendingTerminationContractsCount FROM ContractsEntity WHERE Approver3Status = 6;
-	SELECT COUNT(*) AS ExpiredContractsCount FROM ContractsEntity WHERE Approver3Status = 5;
-	SELECT COUNT(*) AS ActiveContractsCount FROM ContractsEntity WHERE Approver3Status = 2;
-	SELECT COUNT(*) AS TerminatedContractsCount FROM ContractsEntity WHERE Approver3Status = 4;
+	-- Declaration
+	declare @AllContractsCount int
+	declare @PendingApprovalContractsCount int
+	declare @PendingTerminationContractsCount int
+	declare @ExpiredContractsCount int
+	declare @ActiveContractsCount int
+	declare @TerminatedContractsCount int
+
+	-- Initialization
+	SELECT @AllContractsCount = COUNT(*) FROM ContractsEntity;
+	SELECT @PendingApprovalContractsCount = COUNT(*) FROM ContractsEntity WHERE Approver3Status = 1;
+	SELECT @PendingTerminationContractsCount = COUNT(*) FROM ContractsEntity WHERE Approver3Status = 6;
+	SELECT @ExpiredContractsCount = COUNT(*) FROM ContractsEntity WHERE Approver3Status = 5;
+	SELECT @ActiveContractsCount = COUNT(*) FROM ContractsEntity WHERE Approver3Status = 2;
+	SELECT @TerminatedContractsCount = COUNT(*) FROM ContractsEntity WHERE Approver3Status = 4;
+
+	-- Execution and Output Display
+	SELECT @AllContractsCount AS AllContractsCount,
+		   @PendingApprovalContractsCount AS PendingApprovalContractsCount,
+		   @PendingTerminationContractsCount AS PendingTerminationContractsCount,
+		   @ExpiredContractsCount AS ExpiredContractsCount,
+		   @ActiveContractsCount AS ActiveContractsCount,
+		   @TerminatedContractsCount AS TerminatedContractsCount
 END
 EXEC SP_ContractsCounts
