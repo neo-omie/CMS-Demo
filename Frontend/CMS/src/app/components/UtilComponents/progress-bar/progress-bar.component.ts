9fa97920865f775d7386cffd8e0ee1fa0e761f06
Neo-Shreekant02 <shreekant.panigrahi@neosoftmail.com>
2025-05-23 13:13:20 +0530

    some chnages for trigger3

--- Frontend/CMS/src/app/components/UtilComponents/progress-bar/progress-bar.component.ts
+++ Frontend/CMS/src/app/components/UtilComponents/progress-bar/progress-bar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { HelloComponent } from './hello.component';
+import { AppComponent } from '../../../app.component';
 
 @Component({
   selector: 'app-progress-bar',
   standalone: true,
-  imports: [],
+  imports: [BrowserModule, FormsModule, MatButtonModule, MatIconModule, HelloComponent],
   templateUrl: './progress-bar.component.html',
   styleUrl: './progress-bar.component.css'
 })
 export class ProgressBarComponent {
+  step = 0;
 
+  ngOnInit() {}
+
+  startStep() {
+    this.step = this.step + (this.step < 4 ? 1 : 0);
+  }
 }
--- Frontend/CMS/src/app/components/master-employee/master-employee.component.ts
+++ Frontend/CMS/src/app/components/master-employee/master-employee.component.ts
@@ -14,6 +14,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { LoaderComponent } from '../UtilComponents/loader/loader.component';
+import { DecodeToken } from '../../utils/decodeToken';
 
 @Component({
   selector: 'app-master-employee',
@@ -273,9 +274,9 @@ onSubmit(){
 
       addFormValues.email =this.addEmployeeForm.value.email;
       addFormValues.employeeExtension =this.addEmployeeForm.value.employeeExtension;
-      // addFormValues.loggedBy =this.addEmployeeForm.value.loggedBy;
+      //addFormValues.loggedBy =this.addEmployeeForm.value.loggedBy;
 
-      const loggedInEmpCode=localStorage.getItem('empCode');
+      const loggedInEmpCode=DecodeToken.ECode;
       if(!loggedInEmpCode){
         console.log("LoggedInUser EmpCode is not found in localstorage");
         Alert.toast(TYPE.ERROR, true, 'Unable to retrieve logged-in user information.');
