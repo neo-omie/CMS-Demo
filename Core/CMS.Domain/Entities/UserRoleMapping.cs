40905e2ca8fb2a1edaece4c1a5a8a77fba66aa8d
Sakthish-Jayakumar-Nadar <sakthishjayakumarnadar007@gmail.com>
2025-06-10 10:57:42 +0530

    adding role and userRole mapping table

---
+++ Core/CMS.Domain/Entities/UserRoleMapping.cs
@@ -0,0 +1,11 @@
+﻿namespace CMS.Domain.Entities
+{
+    public class UserRoleMapping
+    {
+        public int UserRoleId { get; set; }
+        public int RoleId { get; set; }
+        public string EmployeeCode { get; set; }
+        public Role Role { get; set; }
+        public MasterEmployee Employee { get; set; }
+    }
+}
