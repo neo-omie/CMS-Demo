35e3026602c5d8f73ae4e3a75a3ec9ddfb7d9872
Sakthish-Jayakumar-Nadar <sakthishjayakumarnadar007@gmail.com>
2025-06-11 13:17:33 +0530

    globle error handler

---
+++ Frontend/CMS/src/app/utils/errorHandler.ts
@@ -0,0 +1,27 @@
+import { TYPE } from "../components/auth/login/values.constants";
+import { Alert } from "./alert";
+
+export class ErrorHandler {
+    static handle(error: any) {
+        let errorMessage;
+        if (error.status == 401) {
+            errorMessage = error.error;
+        }
+        else if (error.title !== undefined) {
+            errorMessage = error.title;
+        }
+        else if (error.error !== undefined && error.error.title !== undefined) {
+            errorMessage = error.error.title
+        }
+        else if (error.message !== undefined) {
+            errorMessage = error.message;
+        }
+        else if (error.error !== undefined && error.error.message !== undefined) {
+            errorMessage = error.error.message;
+        }
+        else {
+            errorMessage = "Something went wrong."
+        }
+        Alert.toast(TYPE.ERROR, true, errorMessage);
+    }
+}
