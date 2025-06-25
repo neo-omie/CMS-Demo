bb3d754f333d5abe4a3eafe5a7de276fe74a2a0b
Om <autiom3123@gmail.com>
2025-06-25 15:19:47 +0530

    adding env in user service

--- Frontend/CMS/src/app/services/auth/user.service.ts
+++ Frontend/CMS/src/app/services/auth/user.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 import { AuthResponse, Login, PasswordRenewal } from '../../models/auth/login';
 import { Observable } from 'rxjs';
 import { DecodeToken } from '../../utils/decodeToken';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = "https://localhost:7041/api/Auth";
+  private apiUrl = `${environment.apiUrl}/Auth`;
   constructor(private http:HttpClient) { }
   login(authReq:Login):Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.apiUrl}/login`, authReq);
